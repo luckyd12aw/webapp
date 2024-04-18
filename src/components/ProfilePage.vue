8e842cb5f864e44a93bf39312aee0e7be7d3cb36
solidity12 <volibear1001@gmail.com>
2024-04-18 14:56:23 +0900

    build: Add luckydraw vue

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,14 +1,185 @@
 <template>
-  <h1>"Profile"</h1>
-  <h1>"Profile"</h1>
-  <h1>"Profile"</h1>
-  <h1>"Profile"</h1>
-  <h1>"Profile"</h1>
-  <h1>"Profile"</h1>
-  <h1>"Profile"</h1>
+  <!-- details -->
+  <div class="details">
+    <div class="details-head">4 NFT</div>
+    <div class="details-sub">Discover the winning NFTs from the Lucky Draw platform!</div>
+  </div>
+  <div class="nft-continer">
+    <div class="my-nft-container">
+      <!-- nft1 -->
+      <div class="my-nft-wrapper">
+        <div class="my-nft-price-container">
+          <img class="eth-img" src="../assets/eth.png" alt="eth">
+          <div class="nft-price-text">0.11 ETH</div>
+        </div>
+        <div class="my-nft-title-container">
+          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
+          <div class="my-nft-text">LuckyDraw #3</div>
+          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
+                fill="white" />
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
+                fill="white" />
+            </svg>
+          </button>
+        </div>
+        <img class="my-nft-img" src="../assets/nft.png" alt="">
+      </div>
+      <!-- nft2 -->
+      <div class="my-nft-wrapper">
+        <div class="my-nft-price-container">
+          <img class="eth-img" src="../assets/eth.png" alt="eth">
+          <div class="nft-price-text">0.11 ETH</div>
+        </div>
+        <div class="my-nft-title-container">
+          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
+          <div class="my-nft-text">LuckyDraw #3</div>
+          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
+                fill="white" />
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
+                fill="white" />
+            </svg>
+          </button>
+        </div>
+        <img class="my-nft-img" src="../assets/nft.png" alt="">
+      </div>
+      <!-- nft3 -->
+      <div class="my-nft-wrapper">
+        <div class="my-nft-price-container">
+          <img class="eth-img" src="../assets/eth.png" alt="eth">
+          <div class="nft-price-text">0.11 ETH</div>
+        </div>
+        <div class="my-nft-title-container">
+          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
+          <div class="my-nft-text">LuckyDraw #3</div>
+          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
+                fill="white" />
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
+                fill="white" />
+            </svg>
+          </button>
+        </div>
+        <img class="my-nft-img" src="../assets/nft.png" alt="">
+      </div>
+      <!-- nft4 -->
+      <div class="my-nft-wrapper">
+        <div class="my-nft-price-container">
+          <img class="eth-img" src="../assets/eth.png" alt="eth">
+          <div class="nft-price-text">0.11 ETH</div>
+        </div>
+        <div class="my-nft-title-container">
+          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
+          <div class="my-nft-text">LuckyDraw #3</div>
+          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
+                fill="white" />
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
+                fill="white" />
+            </svg>
+          </button>
+        </div>
+        <img class="my-nft-img" src="../assets/nft.png" alt="">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 </script>
 
-<style scoped></style>
+<style scoped>
+.my-nft-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+  width: 720px;
+}
+
+.my-nft-wrapper {
+  position: relative;
+
+  width: 220px;
+  height: 220px;
+  background: rgba(253, 239, 230, 0.8);
+  border-radius: 8px;
+  margin: 10px;
+}
+
+.my-nft-price-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  margin-top: 8px;
+  margin-left: 8px;
+
+  padding: 4px;
+
+  position: absolute;
+  top: 12px;
+  left: 12px;
+  background-color: white;
+  border-radius: 20px;
+}
+
+.my-nft-title-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+
+  position: absolute;
+  margin-left: 10px;
+  top: 170px;
+  border-radius: 8px;
+  width: 200px;
+  height: 40px;
+
+  background: rgba(216, 148, 60, 0.7);
+  backdrop-filter: blur(6.25px);
+  border-radius: 0px 0px 8px 8px;
+
+  z-index: 1;
+}
+
+.my-website-button {
+  background-color: transparent;
+  border-color: transparent;
+
+  cursor: pointer;
+}
+
+.my-nft-text {
+  font-size: 18px;
+  color: white;
+}
+
+.my-arb-img {
+  margin: 0px 10px;
+  width: 24px;
+}
+
+.my-nft-img {
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+
+  border-radius: 8px;
+}
+</style>
