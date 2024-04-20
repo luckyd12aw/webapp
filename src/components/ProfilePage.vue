aaaceff7bdb3da9296db8bbd845376dae0446497
lukepark327 <lukepark327@gmail.com>
2024-04-21 01:40:12 +0900

    feat: mypage

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,104 +1,230 @@
 <template>
   <!-- details -->
   <div class="details">
-    <div class="details-head">4 NFT</div>
-    <div class="details-sub">Discover the winning NFTs from the Lucky Draw platform!</div>
+    <div class="details-head">{{ (nfts && nfts.length) || 0 }} NFT</div>
+    <div class="details-sub">
+      Discover the winning NFTs from the Lucky Draw platform!
+    </div>
   </div>
   <div class="nft-continer">
     <div class="my-nft-container">
-      <!-- nft1 -->
-      <div class="my-nft-wrapper">
-        <div class="my-nft-price-container">
-          <img class="eth-img" src="../assets/eth.png" alt="eth">
-          <div class="nft-price-text">0.11 ETH</div>
+      <div class="my-nft-wrapper" v-for="nft in nfts" :key="nft.tokenId">
+        <div class="my-nft-wrapper">
+          <div class="my-nft-price-container">
+            <img class="eth-img" src="../assets/eth.png" alt="eth" />
+            <div class="nft-price-text">
+              {{
+                (nft.tokenId !== undefined &&
+                  descriptions[nft.tokenId].price) ||
+                "0.0000"
+              }}
+              ETH
+            </div>
+          </div>
+          <div class="my-nft-title-container" v-if="nft.isClaimed">
+            <img class="my-arb-img" src="../assets/arb.png" alt="arb" />
+            <div class="my-nft-text">
+              LuckyDraw #{{
+                (nft.tokenId !== undefined && nft.tokenId.toString()) || "-1"
+              }}
+            </div>
+            <a
+              target="_blank"
+              href="https://sepolia.arbiscan.io/address/0x0CFADaB77eC10CB761E11ed15E99d1e117B25769"
+            >
+              <button class="my-website-button">
+                <svg
+                  width="22"
+                  height="22"
+                  viewBox="0 0 25 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
+                    fill="white"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+            </a>
+          </div>
+          <div
+            class="my-nft-claim-container"
+            v-if="!nft.isClaimed"
+            @click="getNFT(nft.tokenId)"
+          >
+            <div class="my-nft-claim-text">
+              {{ getLoading }} #{{
+                (nft.tokenId !== undefined && nft.tokenId.toString()) || "-1"
+              }}
+            </div>
+          </div>
+          <img class="my-nft-img" src="../assets/nft.png" alt="" />
         </div>
-        <div class="my-nft-title-container">
-          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
-          <div class="my-nft-text">LuckyDraw #3</div>
-          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
-                fill="white" />
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
-                fill="white" />
-            </svg>
-          </button>
-        </div>
-        <img class="my-nft-img" src="../assets/nft.png" alt="">
-      </div>
-      <!-- nft2 -->
-      <div class="my-nft-wrapper">
-        <div class="my-nft-price-container">
-          <img class="eth-img" src="../assets/eth.png" alt="eth">
-          <div class="nft-price-text">0.11 ETH</div>
-        </div>
-        <div class="my-nft-title-container">
-          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
-          <div class="my-nft-text">LuckyDraw #3</div>
-          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
-                fill="white" />
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
-                fill="white" />
-            </svg>
-          </button>
-        </div>
-        <img class="my-nft-img" src="../assets/nft.png" alt="">
-      </div>
-      <!-- nft3 -->
-      <div class="my-nft-wrapper">
-        <div class="my-nft-price-container">
-          <img class="eth-img" src="../assets/eth.png" alt="eth">
-          <div class="nft-price-text">0.11 ETH</div>
-        </div>
-        <div class="my-nft-title-container">
-          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
-          <div class="my-nft-text">LuckyDraw #3</div>
-          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
-                fill="white" />
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
-                fill="white" />
-            </svg>
-          </button>
-        </div>
-        <img class="my-nft-img" src="../assets/nft.png" alt="">
-      </div>
-      <!-- nft4 -->
-      <div class="my-nft-wrapper">
-        <div class="my-nft-price-container">
-          <img class="eth-img" src="../assets/eth.png" alt="eth">
-          <div class="nft-price-text">0.11 ETH</div>
-        </div>
-        <div class="my-nft-title-container">
-          <img class="my-arb-img" src="../assets/arb.png" alt="arb">
-          <div class="my-nft-text">LuckyDraw #3</div>
-          <button class="my-website-button"><svg width="22" height="22" viewBox="0 0 25 25" fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M3.90625 5.98956H13.3815C13.8129 5.98956 14.1627 5.63979 14.1627 5.20831C14.1627 4.77684 13.8129 4.42706 13.3815 4.42706H3.75C2.97335 4.42706 2.34375 5.05667 2.34375 5.83331V21.25C2.34375 22.0267 2.97335 22.6563 3.75 22.6563H19.1667C19.9433 22.6563 20.5729 22.0267 20.5729 21.25V11.6186C20.5729 11.1871 20.2231 10.8373 19.7917 10.8373C19.3602 10.8373 19.0104 11.1871 19.0104 11.6186V21.0938H3.90625V5.98956Z"
-                fill="white" />
-              <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M22.0137 2.88812C21.8671 2.7416 21.6685 2.6593 21.4612 2.6593H15.8423C15.4109 2.6593 15.0611 3.00907 15.0611 3.44055C15.0611 3.87202 15.4109 4.2218 15.8423 4.2218H19.5751L13.5432 10.2538C13.2381 10.5589 13.2381 11.0536 13.5432 11.3587C13.8482 11.6638 14.3428 11.6638 14.648 11.3587L20.6799 5.32666V9.05942C20.6799 9.49088 21.0297 9.84067 21.4612 9.84067C21.8927 9.84067 22.2424 9.49088 22.2424 9.05942V3.44055C22.2424 3.23335 22.1601 3.03463 22.0137 2.88812Z"
-                fill="white" />
-            </svg>
-          </button>
-        </div>
-        <img class="my-nft-img" src="../assets/nft.png" alt="">
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+import { ethers } from "ethers";
+import descriptions from "../assets/descriptions.json";
+
+const contractAddress = "0x0CFADaB77eC10CB761E11ed15E99d1e117B25769";
+const abi = [
+  {
+    inputs: [],
+    name: "allMetadata",
+    outputs: [
+      {
+        components: [
+          { internalType: "uint256", name: "price", type: "uint256" },
+          { internalType: "uint256", name: "startTime", type: "uint256" },
+          { internalType: "uint256", name: "endTime", type: "uint256" },
+          { internalType: "uint256", name: "annTime", type: "uint256" },
+          { internalType: "address", name: "winner", type: "address" },
+          { internalType: "address[]", name: "candidates", type: "address[]" },
+        ],
+        internalType: "struct Metadata[]",
+        name: "metas",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "buy",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "reward",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+// const provider = new ethers.JsonRpcProvider(
+//   "https://arbitrum-sepolia.blockpi.network/v1/rpc/public"
+// );
+
+let contractWithSigner;
+const signerAddress = ref(ethers.ZeroAddress);
+
+// NFTs
+const nfts = ref([]);
+const getLoading = ref("Claim 🎁");
+
+async function fetchNFTData() {
+  const allMetadata = await contractWithSigner.allMetadata();
+  const signerNfts = [];
+
+  for (const [idx, nft] of allMetadata.entries()) {
+    if (nft.winner.toLowerCase() === signerAddress.value.toLowerCase()) {
+      const isClaimed = await (async () => {
+        try {
+          await contractWithSigner.reward.staticCall(idx);
+          return false;
+        } catch (error) {
+          return true;
+        }
+      })();
+
+      signerNfts.push({
+        tokenId: idx,
+        price: nft.price,
+        startTime: nft.startTime,
+        endTime: nft.endTime,
+        annTime: nft.annTime,
+        winner: nft.winner,
+        candidates: nft.candidates,
+        isClaimed: isClaimed,
+      });
+    }
+  }
+  nfts.value = signerNfts;
+  console.log(nfts.value);
+}
+
+const getNFT = async (tokenId) => {
+  if (contractWithSigner) {
+    getLoading.value = "Claim 💭";
+    try {
+      const tx = await contractWithSigner.reward(tokenId);
+      const res = await tx.wait();
+      // console.log(res.hash);
+      res;
+    } catch (error) {
+      console.error(error);
+    }
+    getLoading.value = "Claim 🎁";
+    nfts.value.isClaimed = true;
+  }
+};
+
+// Mount & Unmount
+let intervalId;
+onMounted(async () => {
+  // First try
+  try {
+    const providerWithSigner = new ethers.BrowserProvider(window.ethereum);
+    const signer = await providerWithSigner.getSigner();
+    // console.log("Account:", await signer.getAddress());
+    signerAddress.value = await signer.getAddress();
+    contractWithSigner = new ethers.Contract(contractAddress, abi, signer);
+
+    await fetchNFTData();
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Try ...
+  intervalId = setInterval(async () => {
+    // Provider Settings
+    if (signerAddress.value === ethers.ZeroAddress) {
+      try {
+        const providerWithSigner = new ethers.BrowserProvider(window.ethereum);
+        const signer = await providerWithSigner.getSigner();
+        // console.log("Account:", await signer.getAddress());
+        signerAddress.value = await signer.getAddress();
+        contractWithSigner = new ethers.Contract(contractAddress, abi, signer);
+
+        await fetchNFTData();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, 5000);
+});
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
 </script>
 
 <style scoped>
@@ -157,6 +283,27 @@
 
   z-index: 1;
 }
+.my-nft-claim-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+
+  position: absolute;
+  margin-left: 10px;
+  top: 170px;
+  border-radius: 8px;
+  width: 200px;
+  height: 40px;
+
+  background: rgba(216, 148, 60, 0.7);
+  backdrop-filter: blur(6.25px);
+  border-radius: 0px 0px 8px 8px;
+
+  z-index: 1;
+
+  cursor: pointer;
+}
 
 .my-website-button {
   background-color: transparent;
@@ -166,6 +313,11 @@
 }
 
 .my-nft-text {
+  font-size: 18px;
+  color: white;
+}
+.my-nft-claim-text {
+  width: 100%;
   font-size: 18px;
   color: white;
 }
